1cd92e38df0ea238ac02408790e1ab6267e22146
Gernot Glawe <gglawe@tecracer.de>
2024-10-29 16:21:46 +0100

    Module updates

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module switchaws
 
 go 1.17
 
-require github.com/go-ini/ini v1.66.4 // indirect
+require github.com/go-ini/ini v1.67.0
+
+require github.com/stretchr/testify v1.9.0 // indirect
--- switchawswrapper.sh
+++ switchawswrapper.sh
@@ -2,7 +2,11 @@
 ## Unset all addition vars here
 unset ITERMBADGE
 unset TASKWARRIOR
-
+unset AWS_DEFAULT_REGION
+unset AWS_REGION
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+unset AWS_SESSION_TOKEN
 
 export $(/usr/local/bin/switchaws $1)
 if [[ ! -z "$CHDIR" ]] then
@@ -13,7 +17,7 @@ if [[ ! -z "$ITERMBADGE" ]] then
     printf "\\e]1337;SetBadgeFormat=%s\\a" \\\n  $(echo -n "$ITERMBADGE" | base64)
 fi
 if [[ ! -z "$TASKWARRIOR" ]] then
-    /usr/local/bin/todo start "$TASKWARRIOR" 
+    /usr/local/bin/todo start "$TASKWARRIOR"
 fi
 
 if [[ ! -z "$AWS_DEFAULT_REGION" ]] then
@@ -23,4 +27,3 @@ fi
 if [[ ! -z "$AWS_URL" ]] then
     open "$AWS_URL"
 fi
-
